6ccac2de0424f61f5601c284709900bcfb0d1204
Christian Ezeani <christian@emplug.com>
2017-06-08 11:56:44 +0100

    Event callback attributes added to angular directive

--- src/angular.ce-text-input.js
+++ src/angular.ce-text-input.js
@@ -4,18 +4,32 @@ window.angular&&(function() {
     module.directive('ceTextInput', [function() {
         return {
             restrict: 'E',
-            scope: { type: '=', ngModel: '=' },
+            scope: {
+                type: '=',
+                ngModel: '=',
+                onInit: '&',
+                onChange: '&',
+                onKeyPress: '&',
+                onEnterKey: '&'
+            },
             link: function(scope, element, attr) {
                 if(window.CETextInput) {
                     scope.$editor= new CETextInput(element[0], {
                         type: scope.type,
                         onInit: function(editor) {
                             scope.ngModel= editor.value;
+                            return scope.onInit({'$editor': editor});
                         },
                         onChange: function(e, editor) {
-                            scope.$apply(function() {
-                                scope.ngModel= editor.value;
-                            });
+                            scope.ngModel= editor.value;
+                            scope.$apply();
+                            return scope.onChange({'$event': e, '$editor': editor});
+                        },
+                        onKeyPress: function(e, editor) {
+                            return scope.onKeyPress({'$event': e, '$editor': editor});
+                        },
+                        onEnterKey: function(e, editor) {
+                            return scope.onEnterKey({'$event': e, '$editor': editor});
                         }
                     });
                 }
